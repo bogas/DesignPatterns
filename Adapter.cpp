9495db1965a43a078259b7f69e2d15512386e4f6
bogas <dawidbogazc@gmail.com>
2020-05-07 23:57:32 +0200

    Adapter

---
+++ Adapter.cpp
@@ -0,0 +1,19 @@
+struct Square {
+  int side{0};
+
+  explicit Square(const int side) : side(side) {}
+};
+
+struct Rectangle {
+  virtual int width() const = 0;
+  virtual int height() const = 0;
+
+  int area() const { return width() * height(); }
+};
+
+struct SquareToRectangleAdapter : Rectangle {
+  SquareToRectangleAdapter(const Square &square) {
+    // todo
+  }
+  // todo
+};
