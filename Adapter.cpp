58eab5a58e9ea47daadf8ea2836c68db64a96e4c
Dawid Bogacz <dbogacz@novomatic-tech.com>
2020-05-08 14:20:32 +0200

    Adapter test

--- Adapter.cpp
+++ Adapter.cpp
@@ -1,19 +1,48 @@
+#include "common_design.h"
+#include "testing.h"
+using namespace std;
+
 struct Square {
-  int side{0};
+    int side{0};
 
-  explicit Square(const int side) : side(side) {}
+    explicit Square(const int side) : side(side) {}
 };
 
-struct Rectangle {
-  virtual int width() const = 0;
-  virtual int height() const = 0;
+class MyRectangle {
+public:
+    virtual int width() const = 0;
 
-  int area() const { return width() * height(); }
+    virtual int height() const = 0;
+
+    int area() const { return width() * height(); }
+
+    virtual ~MyRectangle() {
+        cout << "\nDestructing MyRectangle\n";
+    }
 };
 
-struct SquareToRectangleAdapter : Rectangle {
-  SquareToRectangleAdapter(const Square &square) {
-    // todo
-  }
-  // todo
+struct SquareToRectangleAdapter : MyRectangle {
+    SquareToRectangleAdapter(const Square &square) : side(square.side) {
+    }
+
+    int width() const override {
+        return side;
+    }
+
+    int height() const override {
+        return side;
+    }
+    int area() const { cout<<"Derived area\n"; return 0; }
+
+private:
+    int side;
 };
+
+TEST_CASE("", "") {
+    Square sq{13};
+    unique_ptr<MyRectangle> adapter = make_unique<SquareToRectangleAdapter>(sq);
+    REQUIRE(adapter->area() == 169);
+}
+
+
+
---
+++ common_design.h
@@ -0,0 +1,7 @@
+#include <memory>
+#include <string>
+#include <ostream>
+#include <vector>
+#include <iostream>
+using namespace std;
+
