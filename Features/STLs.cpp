e078dcaff485fa1f81766b579684d785e3a6a0db
bogas <dawidbogazc@gmail.com>
2020-05-18 09:54:15 +0200

    09:54

--- Features/STLs.cpp
+++ Features/STLs.cpp
@@ -1,10 +1,35 @@
 #include "../common_design.h"
 
-int main()
-{
-    vector<int> v{1,2,3,4,5,1,2,4,3,77};
-    cout <<accumulate(begin(v), end(v), 0)<<endl;
-    cout <<reduce(begin(v), end(v), 0)<<endl;
-    for(auto& vv: v) cout<<vv<<" ";
+[[unused]] void foo() {}
+
+void print(vector<int> v) {
+  for (auto &vv : v) {
+    cout << vv << " ";
+  }
+  cout << endl;
 }
 
+int main() {
+  vector<int> v{1, 2, 3, 4, 5, 1, 2, 4, 3, 77};
+  cout << accumulate(begin(v), end(v), 0) << endl;
+  print(v);
+
+  transform(begin(v), end(v), begin(v), [](auto a) { return a + 4; });
+  print(v);
+
+  auto y = v;
+  sort(rbegin(y), rend(y));
+  auto last = unique(begin(y), end(y));
+  print(y);
+  y.erase(last, end(y));
+  print(y);
+  auto l = find(begin(v), end(v), 5);
+  // auto r = remove(begin(v), end(v), 5);
+  print(v);
+
+  v.erase(l);
+  print(v);
+
+  foo();
+  return 0;
+}
--- common_design.h
+++ common_design.h
@@ -1,16 +1,16 @@
 #include <algorithm>
+#include <cstdlib>
+#include <ctime>
+#include <exception>
 #include <iostream>
 #include <memory>
+#include <mutex>
 #include <numeric>
 #include <ostream>
 #include <string>
+#include <string_view>
+#include <thread>
 #include <utility>
 #include <vector>
-#include <cstdlib>
-#include <thread>
-#include <ctime>
-#include <mutex>
-#include <exception>
-#include <string_view>
 
 using namespace std;
